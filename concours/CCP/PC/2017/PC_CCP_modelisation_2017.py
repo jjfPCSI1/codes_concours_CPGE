9a48ac89fa0821fed96903b93d559d2b4353c490
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2017-05-21 18:30:34 +0200

    Ajout de la fin du problème.

--- concours/CCP/PC/2017/PC_CCP_modelisation_2017.py
+++ concours/CCP/PC/2017/PC_CCP_modelisation_2017.py
@@ -11,13 +11,14 @@ charges donnée.
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import math
 
 #plt.style.use('classic')
 
 # Exemples que l'on veut exécuter:
 cylindre_infini = True
-
+deviation_electron = True
 
 # Q6
 def nouveau_potentiel(V,rhos,frontiere,i,j):
@@ -115,6 +116,39 @@ def calc_ExEy(Ex,Ey,V,h):
             Ex[i,j] = - (V[ip,j]-V[im,j])/(h*(ip-im))
             Ey[i,j] = - (V[i,jp]-V[i,jm])/(h*(jp-jm))
 
+
+def graphe_isopot(fichier,V_cyl,levels=None):
+    plt.figure(figsize=(cote,cote))
+    im = plt.imshow(V_cyl, interpolation='bilinear', origin='lower',
+                cmap=cm.jet, extent=(0, L, 0, L) )
+    CS = plt.contour(X,Y,V_cyl,levels=levels)
+    plt.clabel(CS,inline=1,fmt='%d')
+    plt.title('Courbes isopotentielles (en volts)')
+    plt.xlabel('$x$ en m')
+    plt.ylabel('$y$ en m')
+    plt.savefig(fichier)
+
+def graphe_pot(fichier,V_cyl):
+        plt.figure(figsize=(cote,cote))
+        plt.plot(x,V_cyl[:,N//2+1])
+        plt.title('Evolution du potentiel $V(x,y=L/2)$ en volts')
+        plt.xlabel('$x$ en m')
+        plt.ylabel('$V$ en volts')
+        plt.xlim((min(x),max(x)))
+        plt.ylim((0,1.05*np.max(V_cyl)))
+        plt.savefig(fichier)
+
+def graphe_Ex(fichier,Ex_cyl):
+        plt.figure(figsize=(cote,cote))
+        plt.plot(x,Ex_cyl[:,N//2+1])
+        plt.title('Evolution du champ $E_x(x,y=L/2)$ en volts/metre')
+        plt.xlabel('$x$ en m')
+        plt.ylabel('$E_x$ en volts par metre')
+        plt.xlim((min(x),max(x)))
+        plt.ylim((1.05*np.min(Ex_cyl),1.05*np.max(Ex_cyl)))
+        plt.savefig(fichier)
+
+
 # Un premier exemple, celui du cylindre infini
 if cylindre_infini:
     print('*'*70)
@@ -160,7 +194,7 @@ if cylindre_infini:
     
     # Faisons la résolution proprement dite
     print('Début des calculs')
-    seuil = 1e-1
+    seuil = 1e-5
     initialise_rhos_cylindre(rhos_cyl)
     initialise_frontiere_cylindre(frontiere_cyl)
     poisson(itere_SOR,V_cyl,rhos_cyl,frontiere_cyl,seuil)
@@ -178,7 +212,6 @@ if cylindre_infini:
     # Ensuite le premier graphique, en contour d'isopotentielles. On va faire 
     # des fonctions car on refait la même chose avec un cylindre légèrement 
     # différent juste après.    
-    import matplotlib.cm as cm
 
     def graphe_rho(fichier,tab_rho):
         plt.figure(figsize=(cote,cote))
@@ -189,16 +222,6 @@ if cylindre_infini:
         plt.savefig(fichier)
     graphe_rho('cyl_rhos.png',rhos_cyl)
 
-    def graphe_isopot(fichier,V_cyl):
-        plt.figure(figsize=(cote,cote))
-        im = plt.imshow(V_cyl, interpolation='bilinear', origin='lower',
-                    cmap=cm.jet, extent=(0, L, 0, L) )
-        CS = plt.contour(X,Y,V_cyl)
-        plt.clabel(CS,inline=1,fmt='%d')
-        plt.title('Courbes isopotentielles (en volts)')
-        plt.xlabel('$x$ en m')
-        plt.ylabel('$y$ en m')
-        plt.savefig(fichier)
     graphe_isopot('cyl_isoV.png',V_cyl)
     # On remarque que les courbes sont circulaires au centre (comme on s'y 
     # attendrait s'il n'y avait que le cylindre uniformément chargé), mais 
@@ -206,30 +229,12 @@ if cylindre_infini:
     # nulles sur la frontière)
     
     # Puis la coupe pour voir l'évolution du potentiel en passant par le centre
-    def graphe_pot(fichier,V_cyl):
-        plt.figure(figsize=(cote,cote))
-        plt.plot(x,V_cyl[:,N//2+1])
-        plt.title('Evolution du potentiel $V(x,y=L/2)$ en volts')
-        plt.xlabel('$x$ en m')
-        plt.ylabel('$V$ en volts')
-        plt.xlim((min(x),max(x)))
-        plt.ylim((0,1.05*np.max(V_cyl)))
-        plt.savefig(fichier)
     graphe_pot('cyl_evoV.png',V_cyl)
     # On a une branche de parabole dans la zone interne au cylindre (dérivée 
     # seconde constante) et des droites dans les parties externes (dérivée 
     # seconde nulle)
 
-    # Et finalement l'évolution du potentiel
-    def graphe_Ex(fichier,Ex_cyl):
-        plt.figure(figsize=(cote,cote))
-        plt.plot(x,Ex_cyl[:,N//2+1])
-        plt.title('Evolution du champ $E_x(x,y=L/2)$ en volts/metre')
-        plt.xlabel('$x$ en m')
-        plt.ylabel('$E_x$ en volts par metre')
-        plt.xlim((min(x),max(x)))
-        plt.ylim((1.05*np.min(Ex_cyl),1.05*np.max(Ex_cyl)))
-        plt.savefig(fichier)
+    # Et finalement l'évolution du champ
     graphe_Ex('cyl_evoEx.png',Ex_cyl)
     # Lui est linéaire là où le potentiel est parabolique à l'intérieur du 
     # cylindre et normalement nul en passant tout au centre (ce que confirme 
@@ -256,4 +261,149 @@ if cylindre_infini:
     graphe_isopot('cyl2_isoV.png',V_cyl)
     graphe_pot('cyl2_evoV.png',V_cyl)
     graphe_Ex('cyl2_evoEx.png',Ex_cyl)
-    # Cela correspond bien aux graphes de l'énoncé (à l'échelle verticale près)
+    # Cela correspond bien aux graphes de l'énoncé 
+
+
+# Deuxième simulation, un électron entre deux plaques d'un condensateur, base 
+# des oscilloscopes ancienne génération
+if deviation_electron:
+    print('*'*70)
+    print('Deviation electron')
+    # Q24: la vitesse est donnée par v0 = sqrt(2*e*V0/m)
+    # Q25: le champ est donné par -2Vp/d
+    # Q26: trajectoire parabolique
+    # Q27: plus de forces => ligne droite
+    # Q28: rhos est nul en tout point (on a des charges surfaciques mais pas volumiques)
+    L = 0.1
+    D = 7e-2
+    d = 2e-2
+    ell = 4e-2
+    N = 100
+    h = L/N
+    V0= 950
+    Vp= 180
+    m = 9.11e-31
+    e = 1.6e-19
+    rhos_osc = np.zeros((N+1,N+1))
+    V_osc    = np.zeros((N+1,N+1))
+    Ex_osc   = np.zeros((N+1,N+1))
+    Ey_osc   = np.zeros((N+1,N+1))
+    frontiere_osc = np.zeros((N+1,N+1),dtype=bool)
+
+    # Q29
+    def initialise_frontiere_condensateur(V,frontiere):
+        """ Les armatures font ici partie de la frontière et il faut donc 
+        imposer la valeur du potentiel sur chacune d'elle. """
+        debut = int((L - D - ell/2) / L * N)
+        fin   = int((L - D + ell/2) / L * N)
+        h1    = N//2 - int(d/2 / L * N) # Signe pour que l'affichage corresponde bien
+        h2    = N//2 + int(d/2 / L * N) # en toute fin avec les histoires de transposée
+        V[debut:fin+1,h2] = Vp
+        V[debut:fin+1,h1] =-Vp
+        frontiere[debut:fin+1,h1] = True
+        frontiere[debut:fin+1,h2] = True
+        frontiere[0,:] = True
+        frontiere[N,:] = True
+        frontiere[:,0] = True
+        frontiere[:,N] = True
+    
+    # Le calcul proprement dit
+    print('Début des calculs')
+    seuil = 1e-5
+    initialise_frontiere_condensateur(V_osc,frontiere_osc)
+    poisson(itere_SOR,V_osc,rhos_osc,frontiere_osc,seuil)
+    calc_ExEy(Ex_osc,Ey_osc,V_osc,h)
+    print('Fin des calculs')
+
+
+    # D'abord les abscisses
+    x = np.linspace(0,L,N+1)
+    y = np.linspace(0,L,N+1)
+    X,Y = np.meshgrid(x,y)
+    cote = 6
+    
+    levels = np.arange(-Vp+10,Vp,20)
+
+    # La transposition est nécessaire car le premier indice est interprété 
+    # comme une ligne donc est vertical alors que dans l'énoncé on travaille 
+    # depuis le début avec le premier indice qui correspond aux x. Ce n'était 
+    # pas embêtant dans l'exemple du cylindre du fait de la symétrie globale 
+    # du système.
+    graphe_isopot('osc_isoV.png',V_osc.transpose(),levels)
+    
+    # Q30: Calcul des composantes du champ en n'importe quel point x ou y
+    def val_Ex(Ex,Ey,x,y,h):
+        i,j = int(x/h),int(y/h)
+        if i+1 >= len(Ex) or j+1 >= len(Ex): return 0 # Au cas où l'on dépasse (pour debug)
+        rx,ry = x-i*h, y-j*h
+        E = Ex[i,j] + ((Ex[i+1,j]-Ex[i,j])*rx + (Ex[i,j+1]-Ex[i,j])*ry)/h
+        return E
+        
+    def val_Ey(Ex,Ey,x,y,h):
+        i,j = int(x/h),int(y/h)
+        if i+1 >= len(Ex) or j+1 >= len(Ex): return 0 # Au cas où l'on dépasse (pour debug)
+        rx,ry = x-i*h, y-j*h
+        return Ey[i,j] + ((Ey[i+1,j]-Ey[i,j])*rx + (Ey[i,j+1]-Ey[i,j])*ry)/h
+    
+    # On définit aussi celles pour le champ théorique sans effets de bords
+    def Ey_theo(Ex,Ey,x,y,h):
+        if x > L - D - ell/2 and x < L - D + ell/2:
+            return -2*Vp / d
+        else:
+            return 0
+
+    def Ex_theo(Ex,Ey,x,y,h):
+        return 0
+    
+    # Q33
+    # Initialisations diverses
+    Npts = 200              # Nombre de points voulus
+    v0 = np.sqrt(2*e*V0/m)  # Vitesse initiale de l'électron
+    ttraversee = L/v0       # Temps pour la traversée horizontale théorique
+    dt = ttraversee / (Npts+1)  # dt associé pour nombre de points voulu
+    # Pour pouvoir proprement faire aussi l'intégration théorique, on va 
+    # encapsuler tout cela dans une fonction annexe à qui on donne les deux 
+    # fonctions qui renvoient le champ électrique attendu au point voulu avec 
+    # les mêmes arguments que val_Ex et val_Ey précédents.
+    def euler_electron(fEx,fEy):
+        lx = np.zeros(Npts)     # Tableau des coordonnées en x        
+        ly = np.zeros(Npts)     # Tableau des coordonnées en y
+        lvx= np.zeros(Npts)     # Tableau des vitesses en x
+        lvy= np.zeros(Npts)     # Tableau des vitesses en y
+        lx[0] = 0
+        ly[0] = L/2             # La hauteur est non nulle
+        lvx[0]= v0              # mais la vitesse est suivant x
+        lvy[0]= 0
+        # Intégration proprement dite
+        for i in range(1,Npts):
+            lx[i] = lx[i-1] + lvx[i-1]*dt
+            ly[i] = ly[i-1] + lvy[i-1]*dt
+            lvx[i]= lvx[i-1]- e/m * fEx(Ex_osc,Ey_osc,lx[i-1],ly[i-1],h)*dt
+            lvy[i]= lvy[i-1]- e/m * fEy(Ex_osc,Ey_osc,lx[i-1],ly[i-1],h)*dt
+        return lx,ly
+    
+    lx_simu,ly_simu = euler_electron(val_Ex,val_Ey)
+    lx_theo,ly_theo = euler_electron(Ex_theo,Ey_theo)
+    
+    # Reste à faire un zouli dessin pour contempler tout cela et comparer à la 
+    # figure de l'énoncé. (toujours les histoires de transposée pour inverser 
+    # x et y à l'affichage)
+    plt.figure(figsize=(cote,cote))
+    #im = plt.imshow(V_osc.transpose(), interpolation='bilinear', origin='lower',
+    #            cmap=cm.jet, extent=(0, L, 0, L) )
+    CS = plt.contour(X,Y,V_osc.transpose(),levels=levels,colors='k')
+    plt.clabel(CS,inline=1,fmt='%d')
+    plt.plot(lx_simu,ly_simu,label='Simulation',linewidth=3)
+    plt.plot(lx_theo,ly_theo,label='Théorie',linewidth=3)
+    plt.legend(loc='best')
+    plt.xlabel('$x$ en m')
+    plt.ylabel('$y$ en m')
+    plt.title("Déviation d'un électron,\ncalcul théorique du cours vs effets de bords")
+    plt.savefig('osc_simulation_complete.png')
+    
+    # On retrouve ce que l'énoncé suggérait à savoir que la version 
+    # "théorique" va plus loin car les effets de bords vont ralentir 
+    # l'électron dans la simulation, le pas de temps étant calculé pour que la 
+    # version théorique puisse aller au bout mais pas l'autre. En tous cas, en 
+    # pratique, l'approximation de l'absence d'effet de bords n'est pas 
+    # franchement correcte et le calcul du cours s'en trouve mis à mal...
